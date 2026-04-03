0717c802e9dc2efb5abdf68dba6e52241d236598
Bilel Saida <lyricson0203@gmail.com>
2026-04-03 21:04:38 +0100

    fix nuxt build directory config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+  buildDir: '.nuxt-build',
   devtools: { enabled: true },
   modules: ['@primevue/nuxt-module'],
   css: [
